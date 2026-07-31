3f1a5e730abba61daef5af08963048343cd8d634
Andrzej Dębicki <realfictionfactory@gmail.com>
2026-07-31 16:44:14 +0200

    show '*' instead of counter on badge

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
         // If count > 0, show a green badge with the number of changes. Otherwise, clear it.
         if (count > 0) {
-            chrome.action.setBadgeText({ text: String(count), tabId: tabId });
+            chrome.action.setBadgeText({ text: String('*'), tabId: tabId });
             chrome.action.setBadgeBackgroundColor({ color: '#00cc00', tabId: tabId });
         } else {
             chrome.action.setBadgeText({ text: '', tabId: tabId });
